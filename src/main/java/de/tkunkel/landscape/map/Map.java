389deddac19f010d6c1063f58440c7bd15c2ffc1
Torsten Kunkel <git@tkunkel.de>
2023-05-26 18:57:46 +0200

    Initial commit

---
+++ src/main/java/de/tkunkel/landscape/map/Map.java
@@ -0,0 +1,3 @@
+public class Map {
+    public MapTile[][] grid;
+}
---
+++ src/main/java/de/tkunkel/landscape/starter/Starter.java
@@ -0,0 +1,7 @@
+package de.tkunkel.landscape.starter;
+
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
