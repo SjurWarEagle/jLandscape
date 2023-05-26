c9508480deb5370bbc47dbff681a1d2e8b6039bc
Torsten Kunkel <git@tkunkel.de>
2023-05-26 18:58:23 +0200

    init

--- src/main/java/de/tkunkel/landscape/map/Map.java
+++ src/main/java/de/tkunkel/landscape/map/Map.java
@@ -1,3 +1,9 @@
+package de.tkunkel.landscape.map;
+
 public class Map {
     public MapTile[][] grid;
+
+    public Map(int width, int height){
+        grid = new MapTile[width][height];
+    }
 }
--- src/main/java/de/tkunkel/landscape/map/MapTile.java
+++ src/main/java/de/tkunkel/landscape/map/MapTile.java
@@ -1,2 +1,4 @@
+package de.tkunkel.landscape.map;
+
 public class MapTile {
 }
